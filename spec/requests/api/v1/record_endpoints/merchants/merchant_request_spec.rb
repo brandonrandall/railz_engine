4b3370d4c065c7229eb92fcfe995f903608e6e43
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-15 13:44:30 -0600

    add show for individual merchant

--- spec/requests/api/v1/record_endpoints/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/record_endpoints/merchants/merchant_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant API' do
   it 'can return all merchants' do
-    merchants_c = create_list(:merchant, 3)
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants.json'
 
@@ -10,5 +10,16 @@ RSpec.describe 'Merchant API' do
 
     expect(response).to be_success
     expect(merchants.count).to eq(3)
+  end
+
+  it 'can return a single merchant' do
+    merch = create(:merchant)
+
+    get "/api/v1/merchants/#{merch.id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant['name']).to eq(merch.name)
   end
 end

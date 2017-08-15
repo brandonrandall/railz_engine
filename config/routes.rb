f70a1e462441637f16d8678f1a892b9aa0a10a99
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-14 19:20:43 -0600

    add ability to search customers by first name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'customers/find', to: 'customers/search#show'
       resources :customers, only: [:index, :show]
     end
   end

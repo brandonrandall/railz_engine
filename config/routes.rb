0ff4ebfc0cb19fc94fdec07af7f7bd728224734a
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-15 08:04:23 -0600

    add random finder to customers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'customers/find', to: 'customers/search#show'
       get 'customers/find_all', to: 'customers/search#index'
+      get 'customers/random', to: 'customers/random#show'
       resources :customers, only: [:index, :show]
     end
   end

0fe2f7a92dcda9985dbb295613e16e36d24a6374
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-17 11:06:31 -0600

    add most_items to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,13 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
+  has_many :invoices
+
+  def self.most_items(quantity = nil)
+    select('merchants.*, sum(invoice_items.quantity) AS item_count')
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.successful)
+      .group(:id)
+      .order('item_count DESC')
+      .limit(quantity)
+  end
 end

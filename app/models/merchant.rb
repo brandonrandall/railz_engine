81dda9425a71b2e542dfc6e65ecac050b507272d
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-16 18:52:13 -0600

    add revenue for individual merchant to merchant API

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,12 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
+
+  has_many :invoices
+
+  def total_revenue
+    invoices
+      .joins(:transactions, :invoice_items)
+      .merge(Transaction.successful)
+      .sum('invoice_items.unit_price * invoice_items.quantity')
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it {is_expected.to validate_presence_of(:name)}
+  describe 'validations' do
+    it {is_expected.to validate_presence_of(:name)}
+  end
+
+  describe 'instance methods' do
+    describe '#total_revenue' do
+      it "returns total revenue for successful transactions" do
+        merchant = create(:merchant)
+        successful_transactions = create_list(:transaction, 3, result: 'success')
+        failed_transactions = create_list(:transaction, 2, result: 'failed')
+        invoice = create(:invoice, merchant_id: merchant.id)
+        invoice2 = create(:invoice, merchant_id: merchant.id)
+        invoice.transactions << successful_transactions
+        invoice2.transactions << failed_transactions
+        invoice.invoice_items << create_list(:invoice_item, 3, quantity: 3, unit_price: 400)
+        invoice2.invoice_items << create_list(:invoice_item, 3, quantity: 3, unit_price: 400)
+
+        result = merchant.total_revenue
+
+        expect(result).to eq(1.08)
+      end
+    end
+  end
 end

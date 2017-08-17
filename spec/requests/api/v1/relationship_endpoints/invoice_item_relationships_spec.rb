979e842139c162daad4878ab3e1e571d08b7050f
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-17 00:18:56 -0600

    add item relationship to invoice_item api

--- spec/requests/api/v1/relationship_endpoints/invoice_item_relationships_spec.rb
+++ spec/requests/api/v1/relationship_endpoints/invoice_item_relationships_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'Invoice_Item Relationship Endpoints' do
     expect(response).to be_success
     expect(result['id']).to eq(invoice.id)
   end
+
+  it "can find the associated item" do
+    item = create(:item)
+    invoice_item = create(:invoice_item, item_id: item.id)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result['id']).to eq(item.id)
+  end
 end

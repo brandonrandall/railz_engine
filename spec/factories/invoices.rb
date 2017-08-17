89a88b81795945fd811cc4bc92dd4c6f54e4a9a0
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-16 22:11:28 -0600

    add invoice_items relationship to invoice api

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -13,5 +13,15 @@ FactoryGirl.define do
         invoice.transactions << create_list(:transaction, evaluator.transaction_count)
       end
     end
+
+    trait :with_invoice_items do
+      transient do
+        invoice_item_count 3
+      end
+
+      after(:create) do |invoice, evaluator|
+        invoice.invoice_items << create_list(:invoice_item, evaluator.invoice_item_count)
+      end
+    end
   end
 end
--- spec/requests/api/v1/relationship_endpoints/invoice_relationship_spec.rb
+++ spec/requests/api/v1/relationship_endpoints/invoice_relationship_spec.rb
@@ -8,9 +8,26 @@ RSpec.describe 'Invoice Relationship Endpoints' do
     get "/api/v1/invoices/#{invoice.id}/transactions"
 
     result = JSON.parse(response.body)
+
     expect(response).to be_success
+    expect(result.count).to eq(3)
     result.each do |transaction|
       expect(transaction['invoice_id']).to eq(invoice.id)
+    end
+  end
+
+  it "can find the associated invoice_items" do
+    invoice = create(:invoice, :with_invoice_items, invoice_item_count: 3)
+    create(:invoice_item)
+
+    get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result.count).to eq(3)
+    result.each do |invoice_item|
+      expect(invoice_item['invoice_id']).to eq(invoice.id)
     end
   end
 end
